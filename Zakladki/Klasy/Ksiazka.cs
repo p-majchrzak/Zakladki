89d2eafde461ecfcf7ad096fe964ab59a9f7cef1
p-majchrzak <p-majchrzak>
2024-05-10 10:25:30 +0200

    Utworzono klasy.

---
+++ Zakladki/Klasy/Ksiazka.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zakladki.Klasy
+{
+    public class Ksiazka
+    {
+        public int ID { get; set; }
+        public string Tytul { get; set; } = default!;
+        public string Opis { get; set; } = default!;
+        public string Autor { get; set; } = default!;
+        public Ksiazka() { }
+        public Ksiazka(int iD, string tytul, string opis, string autor)
+        {
+            ID = iD;
+            Tytul = tytul;
+            Opis = opis;
+            Autor = autor;
+        }
+        public Ksiazka(string tytul, string opis, string autor)
+        {
+            Tytul = tytul;
+            Opis = opis;
+            Autor = autor;
+        }
+    }
+}
---
+++ Zakladki/Klasy/Zakladka.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zakladki.Klasy
+{
+    public class Zakladka
+    {
+        public int ID { get; set; }
+        public int Strona { get; set; }
+        public string Opis { get; set; } = default!;
+        public required int ID_Ksiazki { get; set; } 
+        public Zakladka() { }
+        public Zakladka(int iD, int strona, string opis, int iD_Ksiazki)
+        {
+            ID = iD;
+            Strona = strona;
+            Opis = opis;
+            ID_Ksiazki = iD_Ksiazki;
+        }
+        public Zakladka(int strona, string opis, int iD_Ksiazki)
+        {
+            Strona = strona;
+            Opis = opis;
+            ID_Ksiazki = iD_Ksiazki;
+        }
+    }
+}
