5baca7ea72a0c65733ff634e2823a968335dcc1e
p-majchrzak <148012675+p-majchrzak@users.noreply.github.com>
2024-05-16 19:48:50 +0200

    Utworzono mechanikę usuwania.

--- Zakladki/Klasy/BazaDanych.cs
+++ Zakladki/Klasy/BazaDanych.cs
@@ -1,32 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text.Json;
 
 namespace Zakladki.Klasy
 {
     public class BazaDanych
     {
-        private static string sciezkaKsiazki = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ksiazki.json");
-        private static string sciezkaZakladki = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "zakladki.json");
+        private static string sciezkaKsiazki = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "bazaKsiazki.txt");
+        private static string sciezkaZakladki = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "bazaZakladki.txt");
 
         public static void ZapiszKsiazke(List<Ksiazka> lista)
         {
-            File.WriteAllText(sciezkaKsiazki, JsonSerializer.Serialize(lista));
+            using (StreamWriter writer = new StreamWriter(sciezkaKsiazki))
+            {
+                foreach (Ksiazka ksiazka in lista)
+                {
+                    writer.WriteLine($"{ksiazka.ID},{ksiazka.Tytul},{ksiazka.Opis},{ksiazka.Autor},{ksiazka.DataWydania}");
+                }
+            }
         }
 
         public static List<Ksiazka> OdczytajKsiazki()
         {
+            List<Ksiazka> lista = new List<Ksiazka>();
             if (File.Exists(sciezkaKsiazki))
             {
-                string zawartosc = File.ReadAllText(sciezkaKsiazki);
-                if (!string.IsNullOrEmpty(zawartosc))
+                using (StreamReader reader = new StreamReader(sciezkaKsiazki))
                 {
-                    List<Ksiazka> lista = JsonSerializer.Deserialize<List<Ksiazka>>(zawartosc);
-                    return lista;
+                    string linia;
+                    while ((linia = reader.ReadLine()) != null)
+                    {
+                        string[] dane = linia.Split(',');
+                        int id;
+                        if (int.TryParse(dane[0], out id))
+                        {
+                            DateTime? dataWydania = null;
+                            if (!string.IsNullOrEmpty(dane[4]))
+                            {
+                                dataWydania = DateTime.Parse(dane[4]);
+                            }
+                            Ksiazka ksiazka = new Ksiazka(id, dane[1], dane[2], dane[3], dataWydania);
+                            lista.Add(ksiazka);
+                        }
+                    }
                 }
             }
-            return new List<Ksiazka>();
+            return lista;
         }
 
         public static void UsunKsiazke(Ksiazka ksiazka, List<Ksiazka> lista)
@@ -37,7 +56,13 @@ namespace Zakladki.Klasy
 
         public static void ZapiszZakladke(List<Zakladka> lista)
         {
-            File.WriteAllText(sciezkaZakladki, JsonSerializer.Serialize(lista));
+            using (StreamWriter writer = new StreamWriter(sciezkaZakladki))
+            {
+                foreach (Zakladka zakladka in lista)
+                {
+                    writer.WriteLine($"{zakladka.ID},{zakladka.Strona},{zakladka.Opis},{zakladka.ID_Ksiazki}");
+                }
+            }
         }
 
         public static void UsunZakladke(Zakladka zakladka, List<Zakladka> lista)
@@ -48,16 +73,25 @@ namespace Zakladki.Klasy
 
         public static List<Zakladka> OdczytajZakladke()
         {
+            List<Zakladka> lista = new List<Zakladka>();
             if (File.Exists(sciezkaZakladki))
             {
-                string zawartosc = File.ReadAllText(sciezkaZakladki);
-                if (!string.IsNullOrEmpty(zawartosc))
+                using (StreamReader reader = new StreamReader(sciezkaZakladki))
                 {
-                    List<Zakladka> lista = JsonSerializer.Deserialize<List<Zakladka>>(zawartosc);
-                    return lista;
+                    string linia;
+                    while ((linia = reader.ReadLine()) != null)
+                    {
+                        string[] dane = linia.Split(',');
+                        int id, strona, idKsiazki;
+                        if (int.TryParse(dane[0], out id) && int.TryParse(dane[1], out strona) && int.TryParse(dane[3], out idKsiazki))
+                        {
+                            Zakladka zakladka = new Zakladka(id, strona, dane[2], idKsiazki);
+                            lista.Add(zakladka);
+                        }
+                    }
                 }
             }
-            return new List<Zakladka>();
+            return lista;
         }
     }
 }
